91bb6e2e80661eee64198c3c84efbf54157efe36
Rob Clark <clarkrob13@gmail.com>
2021-08-04 11:19:40 -0400

    Added Model for Clothing Survey

---
+++ youBox/Models/ClothingSurvey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace youBox.Models
+{
+    public class ClothingSurvey
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Question1 { get; set; }
+        public int Question2 { get; set; }
+        public int Question3 { get; set; }
+        public int Question4 { get; set; }
+        public int Question5 { get; set; }
+        public int Question6 { get; set; }
+        public int Question7 { get; set; }
+        [ForeignKey("Subscriber")]
+        public int SubscriberId { get; set; }
+        public Subscriber subscriber { get; set; }
+
+
+    }
+}
